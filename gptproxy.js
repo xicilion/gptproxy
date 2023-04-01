841051dd7896fde17e09606d74aaca8dfecf661e
xicilion <lion@9465.net>
2023-04-02 01:45:33 +0800

    embed vec docs.

--- gptproxy.js
+++ gptproxy.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const ssl = require('ssl');
 const io = require('io');
+const db = require('db');
 const path = require('path');
 const crypto = require('crypto');
 const util = require('util');
@@ -12,6 +13,27 @@ const GITLAB_API_ENTRY = 'git.corp.com';
 const hc = new http.Client();
 const cache = new util.LruCache(100, 60000);
 const timeout = 90000;
+
+function get_embedding(text) {
+    var res = http.post(`https://${OPENAI_API_ENTRY}/v1/embeddings`, {
+        headers: {
+            'Authorization': `Bearer ${OPENAI_API_KEY}`
+        },
+        json: {
+            "input": text,
+            "model": "text-embedding-ada-002"
+        }
+    });
+
+    if (res.statusCode > 300)
+        throw new Error(res.json().error.message);
+
+    return res.json();
+}
+
+const dbconn = db.open('sqlite:docs.db');
+const prompt = `You are a fibjs development assistant, please answer the question strictly based on the following information in the most detail , do not refer to the nodejs documentation and source code`;
+const modules = `fibjs has the following modules built in: ${util.buildInfo().modules.join(',')}`;
 
 const svr = new ssl.Server(
     crypto.loadCert(path.join(__dirname, 'cert.pem')), crypto.loadPKey(path.join(__dirname, 'key.pem')),
@@ -60,12 +82,58 @@ const svr = new ssl.Server(
                     });
                 }
 
+                if (req.address === '/v1/chat/completions') {
+                    var r = req.json();
+                    var sz = r.messages.length;
+
+                    if (r.messages[sz - 1].content.toLowerCase().indexOf('fibjs') >= 0
+                        || r.messages[0].content.toLowerCase().indexOf('fibjs') >= 0) {
+                        var ask_embedding = get_embedding(r.messages[sz - 1].content);
+
+                        var content = [];
+                        var content_tokens = 0;
+
+                        var res = dbconn.execute(`SELECT docs.text, docs.total_tokens, distance FROM doc_index, docs WHERE vec_search(doc_index.vec, "${JSON.stringify(ask_embedding.data[0].embedding)}:50") AND docs.rowid = doc_index.rowid ORDER BY distance`);
+                        console.log('top distance:', res[0].distance);
+                        for (var i = 0; i < res.length; i++) {
+                            if (content_tokens < 2000) {
+                                content.push(res[i].text);
+                                content_tokens += res[i].total_tokens;
+                            }
+                            else
+                                break;
+                        }
+
+                        r.messages = [
+                            {
+                                role: 'system',
+                                content: prompt
+                            },
+                            {
+                                role: 'assistant',
+                                content: modules
+                            },
+                            {
+                                role: 'assistant',
+                                content: content.join('\n')
+                            },
+                            {
+                                role: 'user',
+                                content: r.messages[sz - 1].content
+                            }
+                        ];
+
+                        req.json(r);
+                    }
+                }
+
                 req.setHeader('Host', OPENAI_API_ENTRY);
                 req.sendTo(upconn);
             }
         } finally {
             conn.close(() => { });
-            upconn.close(() => { });
+            if (upconn)
+                upconn.close(() => { });
         }
     });
 
